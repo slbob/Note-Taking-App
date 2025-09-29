0b4f327c52f7b576abd4217c4d27c89e075649c1
slbob <44273800+slbob@users.noreply.github.com>
2025-09-28 18:57:42 -0700

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    console.error('MONGODB_URI not set in environment');
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
