411b820c2127e6bac40e7385363bb3485fef2189
slbob <44273800+slbob@users.noreply.github.com>
2025-09-28 22:55:10 -0700

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../model/User');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
