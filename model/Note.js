fbc4bc4635bb90c90dae34f7ebfaed43d5f24874
slbob <44273800+slbob@users.noreply.github.com>
2025-09-28 22:55:42 -0700

    Update Note.js

--- model/Note.js
+++ model/Note.js
@@ -20,6 +20,11 @@ const noteSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
   { timestamps: true }
 );
