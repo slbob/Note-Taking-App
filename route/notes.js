046fbe51702ccf7bf0b31a06460827efda94a5eb
slbob <44273800+slbob@users.noreply.github.com>
2025-09-28 22:54:15 -0700

    Update notes.js

--- route/notes.js
+++ route/notes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const notesController = require('../controllers/notesController');
+const notesController = require('../controller/notesController');
 
 router.get('/', notesController.getNotes);
 router.get('/:id', notesController.getNoteById);
